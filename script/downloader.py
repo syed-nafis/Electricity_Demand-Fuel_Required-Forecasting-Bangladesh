fea3241eae9ff114ce93923416723a44e2ff04f1
syed-nafis <www.syed.nafis@gmail.com>
2025-10-20 18:37:37 +0600

    fixes mistakes

--- script/downloader.py
+++ script/downloader.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download daily reports from PGCB")
     parser.add_argument("--path", help="Path to the directory where the downloaded files would be saved")
     parser.add_argument("--last_page_number", help="Last page number to download files from", type=int)
-    parser.add_argument("--sele", help="Choose downloader: selenium or bs4", choices=["selenium", "bs4"], default="bs4")
-    #selenium commandline option
-    parser.add_argument("--csv", action="store_true", help="use bs4 downloader")
+    parser.add_argument("--bs4", action="store_true", help="use bs4 downloader")
     parser.add_argument("--selenium", action="store_true", help="use selenium downloader")
 
     args = parser.parse_args()
