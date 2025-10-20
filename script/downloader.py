de73178b6eff3383f8cad564132c145f9b8236d4
syed-nafis <www.syed.nafis@gmail.com>
2025-10-20 18:27:20 +0600

    updated downloader.py command line options

--- script/downloader.py
+++ script/downloader.py
@@ -8,7 +8,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download daily reports from PGCB")
     parser.add_argument("--path", help="Path to the directory where the downloaded files would be saved")
     parser.add_argument("--last_page_number", help="Last page number to download files from", type=int)
-    parser.add_argument("--downloader", help="Choose downloader: selenium or bs4", choices=["selenium", "bs4"], default="bs4")
+    parser.add_argument("--sele", help="Choose downloader: selenium or bs4", choices=["selenium", "bs4"], default="bs4")
+    #selenium commandline option
+    parser.add_argument("--csv", action="store_true", help="use bs4 downloader")
+    parser.add_argument("--selenium", action="store_true", help="use selenium downloader")
 
     args = parser.parse_args()
 
@@ -16,7 +19,9 @@ if __name__ == "__main__":
     last_page_number = args.last_page_number
     base_url = args.base_url
     
-    if args.downloader == "bs4":
+    if args.bs4:
+        print("Using BeautifulSoup downloader : ")
         bs4_downloader(base_url, path, last_page_number)
-    else:
+    if args.selenium:
+        print("Using Selenium downloader : ")
         selenium_downloader(base_url, path, last_page_number)
